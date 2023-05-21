63acf8ec3a9197943505f3ebefa473ed8eb82141
waleedqu <waleedq93@msn.com>
2023-05-21 11:55:35 -0700

    added assertArraysEqual

---
+++ assertArraysEqual.js
@@ -0,0 +1,17 @@
+const eqArrays = function(array1, array2) {
+  if (array1.length !== array2.length) {
+    return false;
+  } for (i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      return false;
+    }
+  } return true;
+}
+
+const assertArraysEqual = function(arr1, arr2) {
+if (eqArrays(arr1, arr2) === false) {
+  console.log(`❌ Assertion Failed: ${arr1} !== ${arr2}`)
+} else if (eqArrays(arr1, arr2) === true) {
+  console.log(`✅ Assertion Passed: ${arr1} === ${arr2}`)
+}
+}
