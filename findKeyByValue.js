be151af37dcd4264d5d46c186dddcae3a463f9ea
waleedqu <waleedq93@msn.com>
2023-05-27 14:38:49 -0700

    added findkeybyvalue

---
+++ findKeyByValue.js
@@ -0,0 +1,26 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
+  } else if (actual !== expected) {
+    console.log(`❌ Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+
+
+
+
+const findKeyByValue = function(object, value) {
+for (let obj in object) {
+  if (object[obj] === value) {
+    return obj;
+    }
+  }
+};
+
+const bestTVShowsByGenre = { 
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
