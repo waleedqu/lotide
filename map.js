754d3beded1215653a82e4fb37a6af82142aa77c
waleedqu <waleedq93@msn.com>
2023-05-28 23:04:20 -0700

    added map function

---
+++ map.js
@@ -0,0 +1,28 @@
+const eqArrays = function(array1, array2) {
+  if (array1.length !== array2.length) {
+    return false;
+  } for (i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      return false;
+    }
+  } return true;
+}
+
+const assertArraysEqual = function(arr1, arr2) {
+if (eqArrays(arr1, arr2) === false) {
+  console.log(`❌ Assertion Failed: ${arr1} !== ${arr2}`)
+} else if (eqArrays(arr1, arr2) === true) {
+  console.log(`✅ Assertion Passed: ${arr1} === ${arr2}`)
+}
+}
+
+
+
+
+const map = function(array, callback) {
+const results = [];
+for (let item of array) {
+results.push(callback(item));
+} 
+return results;
+}
