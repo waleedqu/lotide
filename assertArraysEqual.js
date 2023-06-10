149f34ce53909373260e38f7fed3dd421d284a2f
waleedqu <waleedq93@msn.com>
2023-06-10 12:53:04 -0700

    refactored with modules

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,12 +1,4 @@
-const eqArrays = function(array1, array2) {
-  if (array1.length !== array2.length) {
-    return false;
-  } for (i = 0; i < array1.length; i++) {
-    if (array1[i] !== array2[i]) {
-      return false;
-    }
-  } return true;
-}
+const eqArrays = require('./eqArrays')
 
 const assertArraysEqual = function(arr1, arr2) {
 if (eqArrays(arr1, arr2) === false) {
@@ -15,3 +7,5 @@ if (eqArrays(arr1, arr2) === false) {
   console.log(`✅ Assertion Passed: ${arr1} === ${arr2}`)
 }
 }
+
+module.exports = assertArraysEqual;
---
+++ test/assertArraysEqualTest.js
@@ -0,0 +1,5 @@
+const assertArraysEqual = require('../assertArraysEqual')
+
+assertArraysEqual([1, 2, 3], [1, 2, 3]);
+assertArraysEqual([1, 2, 3], [1, 2, 3])
+assertArraysEqual([1, 2, 2], [1, 2, 3])
